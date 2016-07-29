6fc8ef57334bc1a2099f3e9a0ef7c29973f9dcb1
Serhio77777 <salat_98@mail.ru>
2016-07-29 10:21:43 +0300

    fixed bugs

--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,45 @@
 angular.module('myApp', ['ngStorage'])
-  .controller('NewCtrl', function($scope, location, $localStorage) {
-    $scope.newItem = {};
-    $scope.items = location.getAll();
-    $scope.addItem = function (item) {
-      if (!item) {
-        return;
-      };
-      $scope.items = location.addItem(item);
-      $scope.newItem = {};
-    };
-    $scope.removeItem = function(index) {
-      $scope.items = location.removeItem(index);
-    };
-    $scope.change = "text";
-    $scope.item = location.classChange(name);
-    $scope.classChange = location.classChange;
-})
+    .controller('NewCtrl', function($scope, location, $localStorage) {
+        $scope.newItem = {};
+        $scope.items = location.getAll();
+        $scope.addItem = function (item) {
+            if (!item) {
+                return;
+            };
+            $scope.items = location.addItem(item);
+            $scope.newItem = {};
+        };
+        $scope.removeItem = function(index) {
+            $scope.items = location.removeItem(index);
+        };
+        $scope.classChange = location.classChange;
+    })
 .service('location', function($localStorage) {
-  var items = $localStorage.newItemsSave || [];
-  return {
-        getAll: function() {
-            return items;
-        },
-        removeItem: function(index, item) {
-            items = items.filter(function(el, ind) {
-                return index !== ind;
-            });
-            $localStorage.newItemsSave = items;
-            return items;
+    function getItems() {
+        return $localStorage.newItemsSave || [];
+    };
+    return {
+          getAll: function() {
+              return getItems();
           },
-        addItem: function(item) {
-            items.push(item);
-            $localStorage.newItemsSave = items;
-            return items;
-        },
-        classChange: function(item) {
-          item.checked = !item.checked;
-        }
-    }
+          removeItem: function(index, item) {
+              return $localStorage.newItemsSave = getItems().filter(function(el, ind) {
+                  return index !== ind;
+              });
+            },
+          addItem: function(item) {
+              $localStorage.newItemsSave.push(item);
+              return getItems();
+          },
+          classChange: function(item) {
+              item.checked = !item.checked;
+          }
+      }
 })
 .directive('myEnter', function () {
     return function (scope, element, attrs) {
         element.bind('keydown keypress', function (event) {
-            if(event.which === 13) {
+            if (event.which === 13) {
                 scope.$apply(function (){
                     scope.$eval(attrs.myEnter);
                 });
